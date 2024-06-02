08a272f4c98dc5a78bc5f8cc4f5aa57b07f55386
BAKHTIYOR AKHATOV <85681712+Bakhtiyor7@users.noreply.github.com>
2024-06-03 03:13:30 +0900

    feat: (javascript behind the scenes) regular functions vs arrow functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -16,57 +16,120 @@ function first() {
   return;
 }
 
-// scopes only have access to variables inside it's scope, not outer scope!
-console.log('executing functions');
-console.log(addNum(1, 2));
-// addFunc(2, 2);
+// // scopes only have access to variables inside it's scope, not outer scope!
+// console.log('executing functions');
+// console.log(addNum(1, 2));
+// // addFunc(2, 2);
+//
+// function addNum(a, b) {
+//   return a + b;
+// }
+//
+// const addFunc = function (a, b) {
+//   return a + b;
+// };
+//
+// var addVar = () => {
+//   console.log('delete all');
+// };
+//
+// // this keyword
+// console.log(this);
+//
+// // simple functions get their own this keyword. so it'll log undefinded to the console
+// const calcAge = function (birthYear) {
+//   console.log(2024 - birthYear);
+//   console.log(this);
+// };
+//
+// // arrow keyword doesn't get its own this. so it will log the window to console
+// const calcAgeArrow = birthYear => {
+//   console.log(2024 - birthYear);
+//   console.log(this);
+// };
+//
+// calcAge(1998); // logs age and undefined
+// calcAgeArrow(1998); // logs age and window
+//
+// const baxti = {
+//   name: 'Baxtiyor',
+//   year: 1998,
+//   calcAge: function () {
+//     console.log('this:', this);
+//     console.log(2024 - this.year);
+//   },
+// };
+// baxti.calcAge(); // logs the baxti object
+//
+// const maftuna = {
+//   year: 1999,
+// };
+//
+// maftuna.calcAge = baxti.calcAge; // this is called method borrowing
+// maftuna.calcAge();
+//
+// const f = baxti.calcAge;
+// f(); // undefined. because there is not owner of this function anymore, It's just a reugalar function call
 
-function addNum(a, b) {
+
+'use strict';
+// const jonas = {
+//     firstName: 'Jonas',
+//     year: 1998,
+//     calcAge: function () {
+//         console.log(2024 - this.year)
+//     },
+//
+//     greet: () => console.log(`hey ${this.firstName}`)
+// }
+//
+//
+// jonas.calcAge() // 26'use strict';
+// // const jonas = {
+// //     firstName: 'Jonas',
+// //     year: 1998,
+// //     calcAge: function () {
+// //         console.log(2024 - this.year)
+// //     },
+// //
+// //     greet: () => console.log(`hey ${this.firstName}`)
+// // }
+// //
+// //
+// // jonas.calcAge() // 26
+// // jonas.greet() // undefined
+//
+// // arguments keyword
+// // const addExpr = function (a, b) {
+// //     console.log( arguments);
+// //     return a + b;
+// // };
+// // addExpr(2, 5);
+// // addExpr(2, 5, 8, 12);
+//
+// var addArrow = (a, b) => {
+//     console.log(arguments);
+//     return a + b;
+// };
+// addArrow(2, 5, 8);
+//
+//
+// // Objects VS Primitives
+// jonas.greet() // undefined
+
+// arguments keyword
+// const addExpr = function (a, b) {
+//     console.log( arguments);
+//     return a + b;
+// };
+// addExpr(2, 5);
+// addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
   return a + b;
-}
-
-const addFunc = function (a, b) {
-  return a + b;
 };
+addArrow(2, 5, 8);
 
-var addVar = () => {
-  console.log('delete all');
-};
 
-// this keyword
-console.log(this);
-
-// simple functions get their own this keyword. so it'll log undefinded to the console
-const calcAge = function (birthYear) {
-  console.log(2024 - birthYear);
-  console.log(this);
-};
-
-// arrow keyword doesn't get its own this. so it will log the window to console
-const calcAgeArrow = birthYear => {
-  console.log(2024 - birthYear);
-  console.log(this);
-};
-
-calcAge(1998); // logs age and undefined
-calcAgeArrow(1998); // logs age and window
-
-const baxti = {
-  name: 'Baxtiyor',
-  year: 1998,
-  calcAge: function () {
-    console.log('this:', this);
-    console.log(2024 - this.year);
-  },
-};
-baxti.calcAge(); // logs the baxti object
-
-const maftuna = {
-  year: 1999,
-};
-
-maftuna.calcAge = baxti.calcAge; // this is called method borrowing
-maftuna.calcAge();
-
-const f = baxti.calcAge;
-f(); // undefined. because there is not owner of this function anymore, It's just a reugalar function call
+// Objects VS Primitives
